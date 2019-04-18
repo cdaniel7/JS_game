cf2e7dbc906297a38d6cdff57e9d786297317301
cdaniel7 <chrisd@joinhomebase.com>
2019-04-17 22:54:08 -0700

    Update app.js

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ var overall_score_p0=0;
 var overall_score_p1=0;
 alternate=0;
 var counter=0;
-// Rolling the Dice 
+// Rolling the Dice
 var dice=Math.floor(Math.random() * 6) + 1;
 
-// New Game Click 
+// New Game Click
 document.querySelector('#main_button').addEventListener('click',function(){
-	 
+
 	alternate=0;
 	document.querySelector('#current_score0').textContent='';
 	document.querySelector('#current_score1').textContent='';
@@ -28,7 +28,7 @@ document.querySelector('#main_button').addEventListener('click',function(){
 		document.querySelector("#p1").style.top="244px";
 		document.getElementById('o0').style.visibility="visible";
 		document.getElementById('o1').style.visibility="hidden";
-		
+
 		current_score_0=0;
      current_score_1=0;
      overall_score_p0=0;
@@ -38,9 +38,9 @@ document.querySelector('#main_button').addEventListener('click',function(){
 
 // OnClick Roll
 document.querySelector('#btn').addEventListener('click', function() {
-	
+
 	if(alternate==0){
-		
+
 		dice=Math.floor(Math.random() * 6) + 1;
 		if (dice==1){
 			current_score_0=0;
@@ -48,8 +48,13 @@ document.querySelector('#btn').addEventListener('click', function() {
 			document.getElementById('dice-1').src = 'img/img_' + dice + '.png';
 			document.querySelector('#current_score'+alternate).textContent=current_score_0;
 			document.querySelector('#overallscore'+alternate).textContent=overall_score_p0;
-			document.getElementById('o1').style.visibility="visible";
+			document.getElementById('o1').style.visibility="hidden";
 			document.getElementById('o0').style.visibility="hidden";
+			document.querySelector('#p2').innerHTML ="<h3>Winner!</h3>";
+
+			document.querySelector("#p2").style.top="227px";
+			document.querySelector('#hold_btn').style.visibility="hidden";
+			document.querySelector('#btn').style.visibility="hidden";
 			alternate=1;
 
 
@@ -69,7 +74,7 @@ document.querySelector('#btn').addEventListener('click', function() {
 	}
 
 	}else if(alternate==1) {
-		
+
 		dice=Math.floor(Math.random() * 6) + 1;
 		if (dice==1)
 		{
@@ -78,13 +83,17 @@ document.querySelector('#btn').addEventListener('click', function() {
 			document.getElementById('dice-1').src = 'img/img_' + dice + '.png';
 			document.querySelector('#current_score'+alternate).textContent=current_score_1;
 			document.querySelector('#overallscore'+alternate).textContent=overall_score_p1;
-			document.getElementById('o0').style.visibility="visible";
+			document.getElementById('o0').style.visibility="hidden";
 			document.getElementById('o1').style.visibility="hidden";
+			document.querySelector('#p1').innerHTML= "<h3>Winner!</h3>";
+			document.querySelector("#p1").style.top="224px";
+			document.querySelector('#hold_btn').style.visibility="hidden";
+			document.querySelector('#btn').style.visibility="hidden";
 			alternate=0;
 
 		} else {
 
-		
+
 		dice=Math.floor(Math.random() * 6) + 1;
 	document.getElementById('dice-1').src = 'img/img_' + dice + '.png';
 	current_score_1=current_score_1+dice;
@@ -92,7 +101,7 @@ document.querySelector('#btn').addEventListener('click', function() {
 	if ((current_score_1+overall_score_p1)>34){
 		document.querySelector('#o1').style.visibility="hidden";
 		document.querySelector('#p2').innerHTML ="<h3>Winner!</h3>";
-	
+
 		document.querySelector("#p2").style.top="227px";
 		document.querySelector('#hold_btn').style.visibility="hidden";
 		document.querySelector('#btn').style.visibility="hidden";
@@ -100,12 +109,12 @@ document.querySelector('#btn').addEventListener('click', function() {
 	}
 	}
 }
-	
+
 })
 
 
 
-// // Sending it to current Score  to over all score 
+// // Sending it to current Score  to over all score
 document.querySelector('#hold_btn').addEventListener('click', function() {
 
 	if(alternate==0){
@@ -133,14 +142,3 @@ if(alternate==0){
 	alternate=0
 }
 })
-
-
-
-
-
-
-
-
-
-
-
